06539c1314d85438d1305603c2184035e1bc65c5
shivam <shivam.bajpai@zaggle.in>
2020-09-11 12:47:35 +0530

    Initial Commit

---
+++ newsapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsapp
+//
+//  Created by Shivam on 11/09/20.
+//  Copyright © 2020 Shivam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
