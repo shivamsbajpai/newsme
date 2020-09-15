d841252b6d87bb83a48b4a30c959a909d5dc13b6
shivam <shivam.bajpai@zaggle.in>
2020-09-15 14:15:32 +0530

    activity_indicator_added

--- newsapp/Controller/WebViewController.swift
+++ newsapp/Controller/WebViewController.swift
@@ -9,40 +9,27 @@
 import UIKit
 import WebKit
 
-class WebViewController: UIViewController {
+class WebViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var web: WKWebView!
     var receivedData = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        web.allowsBackForwardNavigationGestures = true
-        let refresh = UIRefreshControl()
-        refresh.addTarget(self, action: #selector(refreshModule(sender:)), for: UIControl.Event.valueChanged)
+        
+        web.navigationDelegate = self
         web.scrollView.bounces = true
-        web.scrollView.addSubview(refresh)
         let url = URL(string: receivedData)
         let request = URLRequest(url: url!)
         DispatchQueue.main.async {
+            self.activityIndicator.startAnimating()
             self.web.load(request)
+            self.activityIndicator.hidesWhenStopped = true
         }
-        
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activityIndicator.stopAnimating()
     }
     
-    @objc func refreshModule(sender: UIRefreshControl) {
-          web.reload()
-          sender.endRefreshing()
-      }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
