be053808a557e5dd8ec0b7688d5eb2405b0bdce6
Shivam <shivam.bajpai@zaggle.in>
2020-10-14 11:48:09 +0530

    renamed to News Me

--- newsapp/Controller/ViewController.swift
+++ newsapp/Controller/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        navigationController?.setNavigationBarHidden(false, animated: false)
     }
 
     @IBAction func clickPressed(_ sender: UIButton) {
