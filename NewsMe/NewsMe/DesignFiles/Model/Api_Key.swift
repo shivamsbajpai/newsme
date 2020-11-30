a94a8203985f872284262149fcfc8441f741b5a7
Shivam <f20160711@hyderabad.bits-pilani.ac.in>
2020-11-30 15:34:41 +0530

    test commit

---
+++ NewsMe/NewsMe/DesignFiles/Model/Api_Key.swift
@@ -0,0 +1,12 @@
+//
+//  Api_Key.swift
+//  newsapp
+//
+//  Created by Shivam on 16/10/20.
+//  Copyright © 2020 Shivam. All rights reserved.
+//
+
+import Foundation
+struct ApiKey{
+    static let api: String = "16d41c4d36c64372b061c1837b00a9ee";
+}
